02054c9f83eb958c1f79529dfdfb32c7c4216138
User Nika  Azizova <nikaazizova64@gmail.com>
2024-06-14 22:29:52 +0400

    add todolist

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import styles from './App.module.scss'
+
+function App() {
+  const [text, setText] = useState("");
+  const [textOnScreen, setTextOnScreen] = useState();
+  const [textError, setTextError] = useState("");
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!text) {
+      setTextError('Необходимо ввести текст!!! =)')
+    } else {
+      setTextError("")
+    }
+    setTextOnScreen(text.toUpperCase())
+    setText("")
+  };
+
+  return (
+    <>
+      <div className={styles.container}>
+        <form className={styles.textBlock} onSubmit={handleSubmit}>
+          <div className={styles.textName}>Введите текст:</div>
+
+          <textarea className={styles.textArea} name="text" value={text}
+            rows="10" cols="45" onChange={(e) => setText(e.target.value)}
+          ></textarea>
+          <p className={styles.errorMessage}>{textError}</p>
+          <button className={styles.btn}>Добавить</button>
+        </form>
+
+        <div className={styles.postContainer}>
+          <p>{textOnScreen}</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
